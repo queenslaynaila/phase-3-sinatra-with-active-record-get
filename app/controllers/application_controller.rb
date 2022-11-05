28a6cafa59038f4e5fcd0edfa01754d06ba0fa8b
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-11-05 13:51:03 +0300

    complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+  get '/games' do
+    games = Game.all
+    games.to_json
+  end
+  get '/games/:id' do
+    game = Game.find(params[:id])
 
-  get '/' do
-    { message: "Hello world" }.to_json
+    game.to_json(include: {reviews: {include: :user}})
   end
 
 end
